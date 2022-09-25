b05b3d3d3c53c75098b5d296076557c9e3bc65c8
Marco Cutecchia <marco.cutecchia@outlook.it>
2022-09-25 14:38:21 +0200

    overclock mcu to 140Mhz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -181,7 +181,7 @@ void SystemClock_Config(void) {
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLN = 140;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
